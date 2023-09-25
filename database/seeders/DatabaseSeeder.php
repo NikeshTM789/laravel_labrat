225ceb922b41bee844bd79d3483eb8e056feb941
DESKTOP-QCFO6Q2\NIKESH <nekesmgr@gmail.com>
2023-09-25 22:20:05 +0545

    installed Spatie/Permission

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,48 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $roles = [
+            'supreme' => ['name' => 'Supreme'],
+            'admin'   => ['name' => 'Admin'],
+            'user'    => ['name' => 'User'],
+        ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $permissions = [
+            ['name' => 'create_user'],
+            ['name' => 'edit_user'],
+            ['name' => 'delete_user'],
+            ['name' => 'create_product'],
+            ['name' => 'edit_product'],
+            ['name' => 'delete_product'],
+            ['name' => 'create_category'],
+            ['name' => 'edit_category'],
+            ['name' => 'delete_category'],
+        ];
+        foreach ($roles as $role) {
+            Role::create($role);
+        }
+
+        foreach ($permissions as $permission) {
+            Permission::create($permission)->syncRoles($roles['admin']);
+        }
+
+
+        (\App\Models\User::factory()->create([
+            'name'     => 'Super Admin',
+            'email'    => 'supreme@dev.com',
+            'password' => bcrypt('password'),
+        ]))->assignRole($roles['supreme']);
+
+        (\App\Models\User::factory()->create([
+            'name'     => 'Administrator',
+            'email'    => 'admin@dev.com',
+            'password' => bcrypt('password'),
+        ]))->syncRoles($roles['admin']);
+
+        (\App\Models\User::factory()->create([
+            'name'     => 'User',
+            'email'    => 'user@dev.com',
+            'password' => bcrypt('password'),
+        ]))->assignRole($roles['user']);
     }
 }
